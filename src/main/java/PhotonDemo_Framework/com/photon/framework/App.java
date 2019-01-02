1d1d06ece18998b0e0031fbe9109e8c52515e91c
pravin131 <praveenkumar_b@photonint.com>
2019-01-02 19:17:03 +0530

    this is a trail

---
+++ src/main/java/PhotonDemo_Framework/com/photon/framework/App.java
@@ -0,0 +1,13 @@
+package PhotonDemo_Framework.com.photon.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
